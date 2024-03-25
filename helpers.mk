487b90598d9dbf932582732e8e2b92cd5dde18f3
Bartłomiej Rapacz <bartlomiej.rapacz@gmail.com>
2024-03-25 03:01:29 +0100

    Moved stlink_serial number to separated file.

--- helpers.mk
+++ helpers.mk
@@ -1,8 +1,10 @@
+MCU_SERIAL := $(shell cat STLINK_SERIAL)
+
 flash: $(BUILD_DIR)/$(TARGET).bin
-	st-flash --serial $(MCU_SERIAL) write $(BUILD_DIR)/$(TARGET).bin 0x08000000
+	st-flash $(STFLASH_ARGS) --serial $(MCU_SERIAL) write $(BUILD_DIR)/$(TARGET).bin 0x08000000
 
 erase:
-	st-flash --serial $(MCU_SERIAL) erase
+	st-flash $(STFLASH_ARGS) --serial $(MCU_SERIAL) erase
 
 reset:
-	st-flash --serial $(MCU_SERIAL) reset
+	st-flash $(STFLASH_ARGS) --serial $(MCU_SERIAL) reset
