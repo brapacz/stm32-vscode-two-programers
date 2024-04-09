1d9b05a74f932720a980f1bacd75713f14ed19a7
Bartłomiej Rapacz <bartlomiej.rapacz@gmail.com>
2024-04-09 12:13:06 +0200

    Added lines limit for incoming data in UART, to avoid flooding loops.

--- test/simple_transmission_test.py
+++ test/simple_transmission_test.py
@@ -19,23 +19,24 @@ stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
 
 class MCU(object):
-    def __init__(self, name, port, startTime):
+    def __init__(self, name, port, startTime, testCase):
         self.startTime = startTime
+        self.testCase = testCase
 
         self.port = port
         self.serial = serial.Serial(port=port, baudrate=115200, bytesize=8, timeout=1, stopbits=serial.STOPBITS_ONE)
         self.name = name
-
-        self.ready = False
-        self.lines = []
+        self.linesLimit = 200
+        self._setUp()
 
         self._read_data_in_background()
 
-
-    def reset(self):
+    def _setUp(self):
         self.ready = False
         self.lines = []
 
+    def reset(self):
+        self._setUp()
         system(f'cd {self.name} && make reset 2> /dev/null > /dev/null')
 
     def close(self):
@@ -61,6 +62,7 @@ class MCU(object):
                 if data:
                     self.lines.append(data)
                     logger.debug('%0.4f: %s %5s: %s' % (time.time()-self.startTime, '<', self.name, data))
+                    self.testCase.assertLessEqual(len(self.lines), self.linesLimit, f'Too much lines from UART, limit is {self.linesLimit}')
             self.serial.close()
 
         self._thread=Thread(target=_thread_main,args=(self,))
@@ -72,10 +74,10 @@ class SimpleTransmissionTest(unittest.TestCase):
     def setUp(self):
         self.startTime = time.time()
 
-        self.big = MCU('big', BIG_UART_DEVICE, self.startTime)
+        self.big = MCU('big', BIG_UART_DEVICE, self.startTime, self)
         self.big.reset()
 
-        self.small = MCU('small', SMALL_UART_DEVICE, self.startTime)
+        self.small = MCU('small', SMALL_UART_DEVICE, self.startTime, self)
         self.small.reset()
 
         self.big.wait_for_string(b'Ready')
